59bb378f3142201af5be796ef2921e65f3586201
burnedsap <4111699+burnedsap@users.noreply.github.com>
2021-10-11 23:02:11 -0400

    Tetris p5.js version

---
+++ tetris_web/shapes.js
@@ -0,0 +1,12 @@
+function boxShape(c) {
+    fill(c);
+    noStroke();
+    ellipse(0, 0, tileWidth - spacer, tileHeight - spacer)
+}
+
+
+function mapFillerShape(c) {
+    fill(c);
+    noStroke();
+    ellipse(0, 0, tileWidth - spacer, tileHeight - spacer)
+}
