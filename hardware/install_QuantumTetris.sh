ac7b7ccf4ab27fe03f7789c25ee768866bd33ab3
olivierbrcknr <25103920+olivierbrcknr@users.noreply.github.com>
2023-03-23 13:50:46 +0100

    Fix installation link for TETRIS_blocks.csv

--- hardware/install_QuantumTetris.sh
+++ hardware/install_QuantumTetris.sh
@@ -1,12 +1,9 @@
 
 # https://github.com/olivierbrcknr/quantum-tetris
 
-
 # Setup Raspberry Pi
 
-
 # Libraries
-
 sudo apt-get install python3-pip
 sudo pip install evdev
 
@@ -17,10 +14,8 @@ sudo pip install evdev
 mkdir -p quantumTetris
 cd quantumTetris
 
-
 # Python files
 curl https://raw.githubusercontent.com/olivierbrcknr/quantum-tetris/main/hardware/quantumTetris.py > quantumTetris.py
-curl https://raw.githubusercontent.com/olivierbrcknr/quantum-tetris/main/hardware/TETRIS_blocks.csv > TETRIS_blocks.csv
-
+curl https://raw.githubusercontent.com/olivierbrcknr/quantum-tetris/main/Tetris/data/TETRIS_blocks.csv > TETRIS_blocks.csv
 
 # Shell files
